1f85214f91ca98e06078598f9bd56aaad905818a
AnhhTuann <tuannguyen25101999@gmail.com>
2024-10-02 11:54:27 +0700

    add testimonial

--- scripts/app.js
+++ scripts/app.js
@@ -9,3 +9,34 @@ window.addEventListener("click", function (e) {
     menu.classList.remove(activeClass);
   }
 });
+
+$(".testimonial-list").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  prevArrow:
+    "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-arrow-left' aria-hidden='true'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-arrow-right' aria-hidden='true'></i></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+      },
+    },
+  ],
+});
